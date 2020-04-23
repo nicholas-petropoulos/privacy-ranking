f514da482eeea7acb46b8e5ca9dee087a0c7238c
Nicholas Petropoulos <n.petropoulos@outlook.com>
2020-04-23 16:14:29 -0500

    add selenium and proxy usage to get all resources

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-import requests
+import json
+from urllib.parse import urlparse
+
+from browsermobproxy import Server
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-# --- Privacy Score ---
-# num# of external urls
-# redirections
-# http urls
-# external urls dling js or html vs image or video
+# Purpose of this script: List all resources (URLs) that
+# Chrome downloads when visiting some page.
 
+full_url = "https://msn.com"
+# used to filter out false positive external sites with subdomains
+domain = full_url[8:]
+chromedriver_location = "" # Path containing the chromedriver
+browsermobproxy_location = "J:\\iCloud Drive\\Development\\Python\\browsermob-proxy-2.1.4\\bin\\browsermob-proxy.bat" # location of the browsermob-proxy binary file (that starts a server)
+chrome_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+###############
 
-# file = open('top_sites.txt', 'r')
-# lines = file.readlines()
-# for site in lines:
-url = 'https://yahoo.com'
-domain = url[8:]
+# Start browsermob proxy
+server = Server(browsermobproxy_location)
+server.start()
+proxy = server.create_proxy()
+
+# Setup Chrome webdriver - note: does not seem to work with headless On
+options = webdriver.ChromeOptions()
+options.binary_location = chrome_location
+
+# Setup proxy to point to our browsermob so that it can track requests
+options.add_argument('--proxy-server=%s' % proxy.proxy)
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(chrome_options=options)
+
+# Now load some page
+proxy.new_har("Example")
+driver.get(full_url)
+
 print(domain)
-html_page = requests.get(url)
-print('Headers: ' + str(requests.get(url).headers))
+
 # read html page
-soup = BeautifulSoup(html_page.text, features="html.parser")
+soup = BeautifulSoup(driver.page_source, features="html.parser")
 internal = []
 others_https = []
 others_http = []
+ext_images = []
+ext_videos = []
+ext_js = []
+redirects = []
 
-img_types = ['png', 'jpg', 'jpeg', 'JPEG', 'webp']
-document_types = ['html', 'htm', 'js']
-
+# make sure to set ext. lower() when parsing
+img_types = ['png', 'jpg', 'jpeg', 'webp', 'gif', 'svg', 'tiff', 'apng']
+vid_types = ['mp4', 'webm', 'mpg', 'mpeg', 'avi', 'wmv', 'flv', 'm4v', 'mov', 'm4p', 'mpv', 'swf']
+# document_types = ['html', 'htm', 'js']
 for link in soup.findAll('a'):
     # possible external - check url compared to page?
     if 'https' in str(link):
@@ -33,22 +59,73 @@ for link in soup.findAll('a'):
             internal.append(link.get('href'))
         elif link.get('href') is not None:
             others_https.append(link.get('href'))
+        elif link.get('img') is not None:
+            # TODO: get images
+            pass
     # internal link - without http or https
     elif link.get('href') is not None and ('http' not in str(link) or 'https' not in str(link)):
         internal.append(link.get('href'))
     # HTTP (non-SSL link)
     elif link.get('href') is not None:
         others_http.append(link.get('href'))
-    # TODO: save to CSV for each?
 
-print('Num Internal Links: ' + str(len(internal)))
-for link in internal:
-    print(link)
+# Load tracker and ad lists
+with open('lists/trackers.json') as f:
+    tracker_list = str(json.load(f))
 
-print('Num External https: ' + str(len(others_https)))
-for link in others_https:
-    print(link)
+ad_list = []
+# read ads_trackers.txt - add to array, strip newline
+with open('lists/ads_trackers.txt') as f:
+    ad_list_file = f.readlines()
+for line in ad_list_file:
+    ad_list.append(line.strip())
 
-print('Num External http: ' + str(len(others_http)))
-for link in others_http:
-    print(link)
+# Print all URLs/resources that were requested - external
+# links_file = open(f'sites\{domain}.txt', 'w')
+entries = proxy.har['log']["entries"]
+for entry in entries:
+    if 'request' in entry.keys():
+        full_url = entry['request']['url']
+        base_url = urlparse(full_url).netloc
+        # combined url
+        new_url = None
+        try:
+            new_url = base_url.split('.')[1] + '.' + base_url.split('.')[2]
+        except IndexError:
+            pass
+        # check for image extensions
+        if any(ext in full_url for ext in img_types):
+            pass
+        elif any(ext in full_url for ext in vid_types):
+            pass
+        elif 'js' in full_url:
+            pass
+        # ads?
+        # (base_url.split('.')[1] in tracker_list)
+        elif new_url is not None and new_url in ad_list:
+            # print(f'match: {base_url}')
+            # print(f'NEW URL: {new_url}')
+            pass
+        # print(entry['request']['url'])
+        # links_file.write(f"\n{entry['request']['url']}")
+
+
+
+# print('Num Internal Links: ' + str(len(internal)))
+# for link in internal:
+#     pass
+#     #print(link)
+#
+# print('Num External https: ' + str(len(others_https)))
+# for link in others_https:
+#     print(link)
+#
+# print('Num External http: ' + str(len(others_http)))
+# for link in others_http:
+#     print(link)
+
+# TODO: save to CSV for each site
+# TODO: site, num_internal, num_external, num_http, num_redirects, num_ext_js, num_ext_img_vid, num_ad_sites
+
+server.stop()
+driver.quit()
